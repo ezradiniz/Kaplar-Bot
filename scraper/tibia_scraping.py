8aced5100e5cc9363694e2f216a89e6afdccd26f
ezradiniz <linux.esdras@gmail.com>
2017-08-11 19:27:40 -0300

    Abstract scraper functions

---
+++ scraper/tibia_scraping.py
@@ -0,0 +1,19 @@
+import urllib.request as req
+from bs4 import BeautifulSoup as bs
+
+CHARACTER_URL = 'https://secure.tibia.com/community/?name='
+WIKI_URL= 'http://tibia.wikia.com/wiki/'
+WORLD_URL = 'https://secure.tibia.com/community/?subtopic=worlds&world='
+
+
+def character(character_name):
+	pass
+
+def character_key(key, character_name):
+	pass
+
+def world(world_name):
+	pass
+
+def character_status(character_name):
+	pass
